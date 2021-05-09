d75456a95da23231e54724ccff245ca0c8d23a15
David Maisonnette Jentjens <davidjentjens@gmail.com>
2021-05-09 18:10:20 -0300

    Adicionar verificação ao listar inscrições
    Adicionar verificação de existência de usuário no banco ao listar inscrições. Teste devidamente alterado.

--- src/modules/businesses/services/ListSubscriptionsService.spec.ts
+++ src/modules/businesses/services/ListSubscriptionsService.spec.ts
@@ -1,37 +1,57 @@
+import AppError from '@shared/errors/AppError';
+
+import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
 import FakeBusinessClientRepository from '../repositories/fakes/FakeBusinessClientRepository';
 
 import ListSubscriptionsService from './ListSubscriptionsService';
 
+let fakeUsersRepository: FakeUsersRepository;
 let fakeBusinessClientRepository: FakeBusinessClientRepository;
 
 let listSubscriptions: ListSubscriptionsService;
 
 describe('ListSubscriptions', () => {
   beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
     fakeBusinessClientRepository = new FakeBusinessClientRepository();
 
     listSubscriptions = new ListSubscriptionsService(
+      fakeUsersRepository,
       fakeBusinessClientRepository,
     );
   });
 
   it('should be able to list subscriptions from a client', async () => {
+    const client = await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
     await fakeBusinessClientRepository.create({
-      client_id: 'client_id',
+      client_id: client.id,
       tier_id: 'tier1_id',
     });
 
     await fakeBusinessClientRepository.create({
-      client_id: 'client_id',
+      client_id: client.id,
       tier_id: 'tier2_id',
     });
 
     const subscriptions = await listSubscriptions.execute({
-      client_id: 'client_id',
+      client_id: client.id,
     });
 
     expect(subscriptions.length).toEqual(2);
-    expect(subscriptions[0].client_id).toEqual('client_id');
-    expect(subscriptions[1].client_id).toEqual('client_id');
+    expect(subscriptions[0].client_id).toEqual(client.id);
+    expect(subscriptions[1].client_id).toEqual(client.id);
+  });
+
+  it('should not be able to list subscriptions from a non-existant client', async () => {
+    expect(
+      listSubscriptions.execute({
+        client_id: 'fake_id',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
--- src/modules/businesses/services/ListSubscriptionsService.ts
+++ src/modules/businesses/services/ListSubscriptionsService.ts
@@ -1,5 +1,8 @@
 import { inject, injectable } from 'tsyringe';
 
+import AppError from '@shared/errors/AppError';
+
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import BusinessClient from '@modules/businesses/infra/typeorm/entities/BusinessClient';
 import IBusinessClientRepository from '@modules/businesses/repositories/IBusinessClientRepository';
 
@@ -10,11 +13,19 @@ interface IRequest {
 @injectable()
 class SubscribeToBusinessService {
   constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
     @inject('BusinessClientRepository')
     private businessClientRepository: IBusinessClientRepository,
   ) {}
 
   public async execute({ client_id }: IRequest): Promise<BusinessClient[]> {
+    const client = await this.usersRepository.findById(client_id);
+
+    if (!client) {
+      throw new AppError('Client does not exist');
+    }
+
     const subscriptions = await this.businessClientRepository.findSubscribed(
       client_id,
     );
